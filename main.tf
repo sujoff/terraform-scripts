4f962a0c3790201a0703bfbddb11a2934d5646e1
sujoff <devopsujan@gmail.com>
2024-06-25 07:45:30 +0545

    VPC Configurations Added

---
+++ main.tf
@@ -0,0 +1,27 @@
+Welcome to my VPC created using Terraform
+This is my first VPC and It'll surely be working fine
+Here we go.
+Here we go.
+Here we go.
+Here we go.
+Here we go.
+Here we go.
+Here we go.
+
+#Step 1: Create VPC
+
+
+
+#Step 2: Create Public Subnet
+
+
+
+#Step 3: Create Private Subnet
+
+
+
+#Step 4: Create Internet Gateway(IGW)
+
+
+
+#Step 5: Create Eoute Table for Public Subnet
